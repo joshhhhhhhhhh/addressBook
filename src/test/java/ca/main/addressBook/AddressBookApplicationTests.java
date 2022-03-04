9cfef5ef0d6d245885247823677302e23a096527
josh <joshblondin@cmail.carleton.ca>
2022-03-04 15:50:05 -0500

    Update Tests

--- src/test/java/ca/main/addressBook/AddressBookApplicationTests.java
+++ src/test/java/ca/main/addressBook/AddressBookApplicationTests.java
@@ -1,13 +1,20 @@
 package ca.main.addressBook;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AddressBookApplicationTests {
 
+	@Autowired
+	private AddressBookViewController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
